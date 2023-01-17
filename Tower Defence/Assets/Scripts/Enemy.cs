28fc38d005f1eb708586f66b7c486c86fe7778e2
nicosare <83720803+nicosare@users.noreply.github.com>
2023-01-17 14:25:33 +0500

    Добавили тестового врага

--- Tower Defence/Assets/Scripts/Enemy.cs
+++ Tower Defence/Assets/Scripts/Enemy.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
 public abstract class Enemy : MonoBehaviour
 {
-    private int health;
-    private int speed;
-    private bool isArmored;
-    [SerializeField] private GameObject way;
+    [SerializeField] protected int health;
+    [SerializeField] protected int speed;
+    [SerializeField] protected bool isArmored;
+
+    private Queue<Transform> way;
+    private Transform target;
+
+    private void Awake()
+    {
+        var Waypoints = GameObject.FindGameObjectWithTag("Way").transform;
+        way = new Queue<Transform>();
+        foreach (Transform point in Waypoints)
+            way.Enqueue(point);
+    }
+
+    private void Start()
+    {
+        target = way.Dequeue();
+    }
 
     public void GetDamsge(int damage, bool isPiercingAttack)
     {
@@ -15,6 +31,7 @@ public abstract class Enemy : MonoBehaviour
             health -= damage;
         else
             Debug.Log("броня не добита"); //Сделать вывод на экран сообщения, что броня не пробита
+
         if (health <= 0)
             Die();
     }
@@ -26,8 +43,18 @@ public abstract class Enemy : MonoBehaviour
 
     private void MoveToPoints()
     {
+        var dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime);
 
+        if (Vector3.Distance(transform.position, target.position) <= 0.01f)
+        {
+            if (way.Count > 0)
+                target = way.Dequeue();
+            else
+                Die();
+        }
     }
+
     private void Update()
     {
         MoveToPoints();
