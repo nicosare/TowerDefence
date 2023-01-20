a90ab0711b148aa4ebaba165f94b8f0567f2e225
BobylevTimofey <71931806+BobylevTimofey@users.noreply.github.com>
2023-01-20 18:17:09 +0500

    Враги атакуют башню и юнитов на дороге

--- Tower Defence/Assets/Scripts/Enemy.cs
+++ Tower Defence/Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,9 +10,15 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField] protected float speed;
     [SerializeField] protected bool isArmored;
     [SerializeField] protected int coinKill;
+    protected bool canAttack = true;
+    protected bool isEndWay = false;
+    [Range(0.1f, 100)]
+    [SerializeField] protected float attackSpeed;
+    public int damage;
 
     private Queue<Transform> way;
-    private Transform target;
+    private Transform wayPointTarget;
+    private IHealth attackTarget;
 
     private void Awake()
     {
@@ -23,7 +30,7 @@ public abstract class Enemy : MonoBehaviour
 
     private void Start()
     {
-        target = way.Dequeue();
+        wayPointTarget = way.Dequeue();
     }
 
     public void GetDamage(int damage, bool isPiercingAttack)
@@ -45,21 +52,50 @@ public abstract class Enemy : MonoBehaviour
 
     private void MoveToPoints()
     {
-        var dir = target.position - transform.position;
+        var dir = wayPointTarget.position - transform.position;
         transform.Translate(dir.normalized * speed * Time.deltaTime);
-        transform.GetChild(0).rotation = Quaternion.Slerp(transform.GetChild(0).rotation, Quaternion.LookRotation(target.position - transform.position), 3 * Time.deltaTime);
+        transform.GetChild(0).rotation = Quaternion.Slerp(transform.GetChild(0).rotation, Quaternion.LookRotation(wayPointTarget.position - transform.position), 3 * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, target.position) <= 0.1f)
+        if (Vector3.Distance(transform.position, wayPointTarget.position) <= 0.1f)
         {
             if (way.Count > 0)
-                target = way.Dequeue();
+                wayPointTarget = way.Dequeue();
             else
-                Die();
+                isEndWay = true;
         }
     }
 
     private void Update()
     {
-        MoveToPoints();
+        if (canAttack)
+        {
+            if (attackTarget != null)
+                StartCoroutine(Attacking());
+            if (!isEndWay)
+                MoveToPoints();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "RoadUnit")
+            attackTarget = other.gameObject.GetComponent<IHealth>();
+        Debug.Log("collision");
+    }
+
+    IEnumerator Attacking()
+    {
+        canAttack = false;
+        if (attackTarget != null)
+        {
+            Attack();
+            yield return new WaitForSeconds(1 / attackSpeed);
+        }
+        canAttack = true;
+    }
+
+    private void Attack()
+    {
+        attackTarget.GetDamage(damage);
     }
 }
