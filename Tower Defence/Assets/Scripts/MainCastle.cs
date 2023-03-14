eaa837c104a176fc8b7dd5ed36167101e4e8b358
BobylevTimofey <71931806+BobylevTimofey@users.noreply.github.com>
2023-03-14 13:14:22 +0500

    Сделал перезагрузку ультимативной атаки

--- Tower Defence/Assets/Scripts/MainCastle.cs
+++ Tower Defence/Assets/Scripts/MainCastle.cs
@@ -17,12 +17,27 @@ public class MainCastle : MonoBehaviour, IHealth
     private AudioSource audioSource;
     [SerializeField] private AudioClip hurtSound;
     public SoundButton sound;
+    private Slider progressBar;
+    private bool isCooldownUltimateAttack;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        progressBar = UltimateAttackButton.transform.GetChild(0).GetComponent<Slider>();
         ultimateBullet = FactionsManager.Instance.ChoosenFaction.UltimateBullet;
         PrintHealthInUI();
+    }
+
+    private void Update()
+    {
+        if (progressBar.value == progressBar.minValue && !isCooldownUltimateAttack)
+        {
+            isCooldownUltimateAttack = false;
+        }
+        else if (isCooldownUltimateAttack)
+        {
+            progressBar.value = progressBar.minValue + progressBar.maxValue - Time.time;
+        }
     }
     public int Health { get => health; set => health = value; }
 
@@ -71,6 +86,10 @@ public class MainCastle : MonoBehaviour, IHealth
             bullet.transform.SetParent(transform);
             bullet.GetComponent<UltimateBullet>().WayPoints = way.WayPoints;
         }
+        isCooldownUltimateAttack = true;
+        progressBar.minValue = Time.time;
+        progressBar.maxValue = Time.time + cooldownTime;
+        progressBar.value = progressBar.maxValue;
         StartCoroutine(Cooldown());
     }
 
